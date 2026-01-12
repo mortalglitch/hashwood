8e0734ca54389494f54186f8b3eb0374ca24cc02
mortalglitch <mortal.glitch@gmail.com>
2026-01-12 12:26:32 -0500

    Moved hashing for directory outside of main to internal/md5_util

---
+++ internal/md5_utils/parse.go
@@ -0,0 +1,41 @@
+package md5utils
+
+import (
+	"crypto/md5"
+	"io"
+	"os"
+)
+
+type HashData struct {
+	Filename string
+	Hash     []byte
+}
+
+func ParseDirectory(target string) ([]HashData, error) {
+	files, err := os.ReadDir(target)
+	if err != nil {
+		return nil, err
+	}
+
+	var results []HashData
+
+	for _, file := range files {
+		f, err := os.Open(target + file.Name())
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+
+		h := md5.New()
+		if _, err := io.Copy(h, f); err != nil {
+			return nil, err
+		}
+
+		results = append(results, HashData{
+			Filename: f.Name(),
+			Hash:     h.Sum(nil),
+		})
+	}
+
+	return results, nil
+}
--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"io"
 	"log"
-	"os"
+
+	md5utils "github.com/mortalglitch/hashwood/internal/md5_utils"
 )
 
 func main() {
 	fmt.Println("Starting up and checking example directory")
 
-	files, err := os.ReadDir("./testing")
+	hashResults, err := md5utils.ParseDirectory("./testing/")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
-		f, err := os.Open("./testing/" + file.Name())
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-
-		h := md5.New()
-		if _, err := io.Copy(h, f); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s - %x\n", f.Name(), h.Sum(nil))
+	for _, hash := range hashResults {
+		fmt.Printf("%s - %x\n", hash.Filename, hash.Hash)
 	}
 }
