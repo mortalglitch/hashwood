6d3a8d53109aaaff28d28265af3fc9f88dfbe20f
mortalglitch <mortal.glitch@gmail.com>
2026-01-16 12:56:20 -0500

    fix - unable to restart server, also added safer shutdown if user exits during server operation

--- command_server.go
+++ command_server.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+var activeServer *http.Server
+
 func (cfg *appConfig) handlerReports(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -31,26 +33,50 @@ func (cfg *appConfig) handlerReports(w http.ResponseWriter, r *http.Request) {
 }
 
 // Need to add a shutdown function.
-func (cfg *appConfig) startServer(words []string, srv *http.Server) error {
+func (cfg *appConfig) CommandServer(words []string) error {
+	const filepathRoot = "."
+	const port = "8080"
+
 	if len(words) > 1 {
 		command := words[1]
 		if command == "start" {
+			if activeServer != nil {
+				fmt.Println("Error: Server is already running.")
+				return nil
+			}
+			mux := http.NewServeMux()
+
+			mux.HandleFunc("GET /report", cfg.handlerReports)
+			activeServer = &http.Server{
+				Addr:    ":" + port,
+				Handler: mux,
+			}
+
 			fmt.Println("Server starting on http://localhost:8080/report")
 			go func() {
-				if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+				if err := activeServer.ListenAndServe(); err != http.ErrServerClosed {
 					fmt.Printf("Server error: %v\n", err)
+					activeServer = nil
 				}
 			}()
 		} else if command == "stop" {
+			if activeServer == nil {
+				fmt.Println("Error: No server is currently running.")
+				return nil
+			}
+
 			fmt.Println("Stopping server...")
 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
 
-			if err := srv.Shutdown(ctx); err != nil {
+			if err := activeServer.Shutdown(ctx); err != nil {
 				fmt.Printf("Shutdown failed: %v\n", err)
 			} else {
 				fmt.Println("Server stopped gracefully.")
 			}
-			cancel()
+
+			activeServer = nil
+
 		}
 	}
 
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"strings"
 
 	_ "github.com/lib/pq"
 
@@ -36,16 +36,6 @@ func main() {
 		db: dbQueries,
 	}
 
-	const filepathRoot = "."
-	const port = "8080"
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /report", cfg.handlerReports)
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
-	}
-
 	for {
 		result := inputoutput.GetInput()
 		if len(result) == 0 {
@@ -71,11 +61,17 @@ func main() {
 				log.Fatal(err)
 			}
 		} else if result[0] == "server" {
-			err := cfg.startServer(result, srv)
+			err := cfg.CommandServer(result)
 			if err != nil {
 				log.Fatal(err)
 			}
 		} else if result[0] == "quit" {
+			// TODO make this cleaner
+			breakCommand := "server stop"
+			err := cfg.CommandServer(strings.Fields(breakCommand))
+			if err != nil {
+				log.Fatal(err)
+			}
 			break
 		}
 	}
